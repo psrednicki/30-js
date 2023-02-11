b9dd4bfce3f41f6cf18b4a0d969dc6c24560e8c2
psrednicki <patryk.srednicki@acaisoft.com>
2023-02-11 20:49:03 +0100

    feat: excercies days of febuary 2022

--- 04_Day_Conditionals/04_day_starter/scripts/foo.js
+++ 04_Day_Conditionals/04_day_starter/scripts/foo.js
@@ -1,3 +1,11 @@
 export const useFoo = (input) => {
-    return console.log("I am from Foo ", input)
+   console.log("I am from Foo ", input)
+}
+
+// Month in JavaScript is 0-indexed (January is 0, February is 1, etc), 
+// but by using 0 as the day it will give us the last day of the prior
+// month. So passing in 1 as the month number will return the last day
+// of January, not February
+export function daysInMonth (month) {
+    return new Date(2022, month, 0).getDate();
 }
--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,7 +1,9 @@
 // this is your main.js script
 
-import { useFoo } from "./foo.js";
+import { useFoo, daysInMonth } from "./foo.js";
 
 console.log("4 DAY")
 
 useFoo("FROM MAIN")
+
+console.log(`In Febuary of 2022 we have ${daysInMonth(2)} days `)
