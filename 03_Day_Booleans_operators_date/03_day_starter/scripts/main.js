d4348fe7ffb119169de17bdfdcf453c1ff506784
psrednicki <patryk.srednicki@acaisoft.com>
2023-02-11 18:31:58 +0100

    day3: booleans and date script

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,37 @@
 // this is your main.js script
+let firstTen = 10
+let secondTen = '10'
+console.log('3 day');
+
+console.log("Is true ===", firstTen === secondTen)
+console.log("Is true ==", firstTen == secondTen)
+
+
+let base = 20
+let height = 10
+
+function getAreaOfTriangle(x, h) {
+    return (h*x)/2
+}
+
+console.log("Area of triagle is ", getAreaOfTriangle(base, height) )
+
+
+let numberOfYear = prompt('Enter your year of born', 'number goes here')
+
+function checkIfMature(year){
+    const curretYear = new Date().getFullYear();
+    console.log(curretYear)
+    if(typeof year === 'number' || typeof year === 'string') {
+        if((curretYear - year) >= 18) {
+            console.log('Year are mature')
+        } else {
+            console.log("NO!")
+        }
+    } else {
+        console.log("Invalid year format")
+    }
+
+}
+
+checkIfMature(numberOfYear)
